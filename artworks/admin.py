3ca639500abe419995244af021b9d57d2cb22461
Botond Ordog <botond.ordog@hotmail.com>
2021-03-20 12:33:33 +0100

    registered models to site

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Artworks, Categories, ArtCategories, ArtFavourites, ArtLikes
 # Register your models here.
+
+
+admin.site.register(Artworks)
+admin.site.register(Categories)
+admin.site.register(ArtCategories)
+admin.site.register(ArtLikes)
+admin.site.register(ArtFavourites)
+
--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Galleries, GalleryArtworks, UserSavedGalleries
 # Register your models here.
+
+
+admin.site.register(Galleries)
+admin.site.register(GalleryArtworks)
+admin.site.register(UserSavedGalleries)
