0d139f1e5185c5384bd1f7a1082b0b196739bda5
Botond Ordog <botond.ordog@hotmail.com>
2021-03-21 10:55:05 +0100

    removed generated comments in admins

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Artwork, Category, ArtCategory, ArtFavourite, ArtLike
 
+
 admin.site.register(Artwork)
 admin.site.register(Category)
 admin.site.register(ArtCategory)
